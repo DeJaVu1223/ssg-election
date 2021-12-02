88389a2aa486b9e98ba9e615c649bac36d8bbd5d
Basam C. Serad <58251967+DeJaVu1223@users.noreply.github.com>
2021-12-02 12:13:56 +0800

    Login Form

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -25,110 +25,24 @@
         </q-card-section>
         <q-card-section>
           <div class="col text-h6 ellipsis flex justify-center">
-            <div class="text-h4 text-primary q-my-none text-weight-bold">LOGIN</div>
+            <div class="text-h4 text-primary q-my-none text-weight-bold">
+              LOGIN
+            </div>
           </div>
         </q-card-section>
 
         <q-card-section>
           <!--login form-->
-          <div class="q-pl-xl q-pr-xl">
-            <q-input
-              v-model="admin_user"
-              dense
-              label="Username"
-              lazy-rules
-              :rules="[(val) => (val && val.length > 0) || 'Input your Username']"
-            >
-              <template v-slot:prepend>
-                <q-icon name="person" />
-              </template>
-            </q-input>
-
-            <q-input
-              v-model="admin_pass"
-              dense
-              label="Password"
-              :type="isPwd ? 'password' : 'text'"
-              lazy-rules
-              :rules="[(val) => (val && val.length > 0) || 'Input your password']"
-            >
-              <template v-slot:append>
-                <q-icon
-                  :name="isPwd ? 'visibility_off' : 'visibility'"
-                  class="cursor-pointer"
-                  @click="isPwd = !isPwd"
-                />
-              </template>
-              <template v-slot:prepend>
-                <q-icon name="lock" />
-              </template>
-            </q-input>
-
-            <div class="flex justify-center">
-              <q-btn
-                :ripple="false"
-                unelevated
-                rounded
-                dense
-                class="glossy q-mt-xs full-width"
-                label="Login"
-                color="primary"
-                @click="adminUser()"
-              />
-            </div>
-
-            <div class="text-center q-mt-sm d-qutter-lg">
-              <router-link class="text-red-4 text-caption" to="/"
-                >forgot password?</router-link
-              >
-            </div>
-          </div>
-        </q-card-section>
-      </q-card>
-    </q-dialog>
-
-    <!----------------------Voter Login----------------------->
-    <q-img class="wave" src="~assets/images/image.png" />
-    <div class="row" style="height: 95vh">
-      <div class="col-8 col-md-6 flex justify-center content-center gt-sm">
-        <q-img src="~assets/images/v.svg" class="responsive" alt="bg-image" />
-      </div>
-
-      <div
-        v-bind:class="{ 'justify-center': $q.screen.md || $q.screen.sm || $q.screen.xs }"
-        class="col-12 col-md-6 flex content-center q-pt-lg"
-      >
-        <q-card
-          class="shadow-20"
-          v-bind:style="$q.screen.lt.sm ? { width: '80%' } : { width: '60%' }"
-        >
-          <q-card-section>
-            <q-avatar size="103px" class="absolute-center shadow-10">
-              <img src="../assets/images/avatar.svg" alt="logo" />
-            </q-avatar>
-          </q-card-section>
-          <q-card-section>
-            <!--title-->
-            <div class="q-pt-lg">
-              <div class="col text-h6 ellipsis flex justify-center">
-                <div class="text-h2 text-primary q-my-none text-weight-bold">SSG</div>
-              </div>
-              <div class="col text-h6 ellipsis flex justify-center">
-                <div class="text-h4 text-primary q-my-none text-weight-bold">
-                  Election
-                </div>
-              </div>
-            </div>
-          </q-card-section>
-          <q-card-section>
-            <!--login form-->
+          <q-form @submit="adminUser">
             <div class="q-pl-xl q-pr-xl">
               <q-input
-                v-model="username"
+                v-model="admin_user"
                 dense
                 label="Username"
                 lazy-rules
-                :rules="[(val) => (val && val.length > 0) || 'Input your Username']"
+                :rules="[
+                  (val) => (val && val.length > 0) || 'Input your Username',
+                ]"
               >
                 <template v-slot:prepend>
                   <q-icon name="person" />
@@ -136,12 +50,14 @@
               </q-input>
 
               <q-input
-                v-model="password"
+                v-model="admin_pass"
                 dense
                 label="Password"
                 :type="isPwd ? 'password' : 'text'"
                 lazy-rules
-                :rules="[(val) => (val && val.length > 0) || 'Input your password']"
+                :rules="[
+                  (val) => (val && val.length > 0) || 'Input your password',
+                ]"
               >
                 <template v-slot:append>
                   <q-icon
@@ -164,24 +80,131 @@
                   class="glossy q-mt-xs full-width"
                   label="Login"
                   color="primary"
-                  @click="loginUser()"
+                  type="submit"
                 />
               </div>
+
               <div class="text-center q-mt-sm d-qutter-lg">
-                <router-link
-                  class="text-red-4 text-caption"
-                  @click="ssg_login"
-                  to="/login_ssg"
-                  >sign-in to ssg account?</router-link
+                <router-link class="text-red-4 text-caption" to="/"
+                  >forgot password?</router-link
                 >
               </div>
-              <div
-                class="q-mt-md text-center text-caption text-weight-medium text-primary absolute-center-right"
-              >
-                *To register your account and retrieve the password, proceed to Division
-                of Student Affairs, Alonto Hall, MSU Marawi Campus.
+            </div>
+          </q-form>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+
+    <!----------------------Voter Login----------------------->
+    <q-img class="wave" src="~assets/images/image.png" />
+    <div class="row" style="height: 95vh">
+      <div class="col-8 col-md-6 flex justify-center content-center gt-sm">
+        <q-img src="~assets/images/v.svg" class="responsive" alt="bg-image" />
+      </div>
+
+      <div
+        v-bind:class="{
+          'justify-center': $q.screen.md || $q.screen.sm || $q.screen.xs,
+        }"
+        class="col-12 col-md-6 flex content-center q-pt-lg"
+      >
+        <q-card
+          class="shadow-20"
+          v-bind:style="$q.screen.lt.sm ? { width: '80%' } : { width: '60%' }"
+        >
+          <q-card-section>
+            <q-avatar size="103px" class="absolute-center shadow-10">
+              <img src="../assets/images/avatar.svg" alt="logo" />
+            </q-avatar>
+          </q-card-section>
+          <q-card-section>
+            <!--title-->
+            <div class="q-pt-lg">
+              <div class="col text-h6 ellipsis flex justify-center">
+                <div class="text-h2 text-primary q-my-none text-weight-bold">
+                  SSG
+                </div>
+              </div>
+              <div class="col text-h6 ellipsis flex justify-center">
+                <div class="text-h4 text-primary q-my-none text-weight-bold">
+                  Election
+                </div>
               </div>
             </div>
+          </q-card-section>
+          <q-card-section>
+            <!--login form-->
+            <q-form @submitssg="loginUser">
+              <div class="q-pl-xl q-pr-xl">
+                <q-input
+                  v-model="username"
+                  dense
+                  label="Username"
+                  lazy-rules
+                  :rules="[
+                    (val) => (val && val.length > 0) || 'Input your Username',
+                  ]"
+                >
+                  <template v-slot:prepend>
+                    <q-icon name="person" />
+                  </template>
+                </q-input>
+
+                <q-input
+                  v-model="password"
+                  dense
+                  label="Password"
+                  :type="isPwd ? 'password' : 'text'"
+                  lazy-rules
+                  :rules="[
+                    (val) => (val && val.length > 0) || 'Input your password',
+                  ]"
+                >
+                  <template v-slot:append>
+                    <q-icon
+                      :name="isPwd ? 'visibility_off' : 'visibility'"
+                      class="cursor-pointer"
+                      @click="isPwd = !isPwd"
+                    />
+                  </template>
+                  <template v-slot:prepend>
+                    <q-icon name="lock" />
+                  </template>
+                </q-input>
+
+                <div class="flex justify-center">
+                  <q-btn
+                    :ripple="false"
+                    unelevated
+                    rounded
+                    dense
+                    class="glossy q-mt-xs full-width"
+                    label="Login"
+                    color="primary"
+                    type="submitssg"
+                  />
+                </div>
+                <div class="text-center q-mt-sm d-qutter-lg">
+                  <router-link
+                    class="text-red-4 text-caption"
+                    @click="ssg_login"
+                    to="/login_ssg"
+                    >sign-in to ssg account?</router-link
+                  >
+                </div>
+                <div
+                  class="
+                    q-mt-md
+                    text-center text-caption text-weight-medium text-primary
+                    absolute-center-right
+                  "
+                >
+                  *To register your account and retrieve the password, proceed
+                  to Division of Student Affairs, Alonto Hall, MSU Marawi
+                  Campus.
+                </div>
+              </div>
+            </q-form>
           </q-card-section>
         </q-card>
       </div>
